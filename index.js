3d080f27f41876989701165949b760b46f262b16
Carl <carlbrainegebe@gmail.com>
2021-05-04 03:01:48 -0500

    Terminer

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(name) {
+    //name = "Nancy";
+    return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name, language) {
+ return `Hi, my name is ${name} and I am learning to program in ${language}.`;   
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;   
+   }
